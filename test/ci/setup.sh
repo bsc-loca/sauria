cfda7eefe6beb960371fc6e07779e34416452d65
jfornt <jordi.fornt@bsc.es>
2025-10-10 08:13:33 +0200

    add python path to ci setup

--- test/ci/setup.sh
+++ test/ci/setup.sh
@@ -1,5 +1,6 @@
 export RTL_DIR=$(pwd)/RTL
 export PULP_DIR=$(pwd)/pulp_platform
+export PYTHON_DIR=$(pwd)/Python
 export TEST_DIR=$(pwd)/test
 export VERILATOR_ROOT=$(pwd)/tools/verilator
 export PATH=$PATH:$VERILATOR_ROOT/bin

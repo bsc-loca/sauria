43c7b4405eae40db151682fddc08f089b8fc5531
jfornt <jordi.fornt@bsc.es>
2025-10-10 07:42:21 +0200

    minor ci fix

--- test/ci/setup.sh
+++ test/ci/setup.sh
@@ -1,5 +1,5 @@
 export RTL_DIR=$(pwd)/RTL
 export PULP_DIR=$(pwd)/pulp_platform
 export TEST_DIR=$(pwd)/test
-export VERILATOR_ROOT=/opt/verilator/4.228/
+export VERILATOR_ROOT=$(pwd)/tools/verilator
 export PATH=$PATH:$VERILATOR_ROOT/bin
